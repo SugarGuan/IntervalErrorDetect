9bfec2f9b563298346245eabc45dfb8f04b67ac5
关运通 <yuntong.guan@gmail.com>
2019-11-06 10:19:45 +0800

    Fill all Index-Documents Relation(23/42).

---
+++ src/main/java/core/learn/module/Iec104Module.java
@@ -0,0 +1,43 @@
+package core.learn.module;
+
+import core.learn.field.CmdField;
+import core.learn.field.CommaddrField;
+import core.learn.field.InfoaddrField;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.Function;
+
+import java.util.*;
+
+public class Iec104Module extends Module {
+
+    @Override
+    public JavaPairRDD<String, Map<String,Object>> retrieveRDD(Map<String, JavaPairRDD<String, Map<String, Object>>> rddMap){
+        return rddMap.get("au_pkt_iec104");
+    }
+
+    @Override
+    public void fieldFillin (Map<String, JavaPairRDD<String, Map<String, Object>>> rddMap) {
+        JavaPairRDD<String, Map<String, Object>> pairRDD = this.retrieveRDD(rddMap);
+
+        if (pairRDD.count() == 0)
+            return ;
+
+        pairRDD.groupByKey().values().map(new Function<Iterable<Map<String, Object>>, Object>() {
+                    List<String> commaddrField;
+                    List<String> infoaddrField;
+                    @Override
+                    public Object call(Iterable<Map<String, Object>> maps) throws Exception {
+                        commaddrField = new ArrayList<>();
+                        infoaddrField = new ArrayList<>();
+                        for (Map<String, Object> map: maps) {
+                            commaddrField.add((String) map.get("i_commaddr"));
+                            infoaddrField.add((String) map.get("i_infoaddr"));
+                        }
+                        CommaddrField.append(commaddrField);
+                        InfoaddrField.append(infoaddrField);
+                        return null;
+                    }
+                }
+        ).collect();
+    }
+}
