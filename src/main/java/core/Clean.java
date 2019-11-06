0e1a297d2869f0a41a1016ae5f35688eba6a1bee
关运通 <yuntong.guan@gmail.com>
2019-11-06 13:30:38 +0800

    Clean Mode added.

--- src/main/java/core/Clean.java
+++ src/main/java/core/Clean.java
@@ -1,4 +1,10 @@
 package core;
 
+import core.clean.CleanRedis;
+
 public class Clean {
+    public void execute() {
+        CleanRedis cleanRedis = new CleanRedis();
+        cleanRedis.cleanRecords();
+    }
 }
---
+++ src/main/java/core/clean/CleanRedis.java
@@ -0,0 +1,7 @@
+package core.clean;
+
+public class CleanRedis {
+    public void cleanRecords() {
+
+    }
+}
